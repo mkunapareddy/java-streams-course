4c143f188359e7bced23da8f1b027ff9265cbaf7
mkunapareddy <https://github.com/mkunapareddy/javaStreams.git>
2021-03-28 18:14:38 -0500

    L1

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -6,6 +6,7 @@ import beans.Person;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +20,21 @@ public class Lecture1 {
   @Test
   public void imperativeApproach() throws IOException {
     List<Person> people = MockData.getPeople();
+    List<Person> youngPeople = new ArrayList<>();
+
+    final int limit = 10;
+    int count = 0;
+
+    for (Person person : people) {
+      if (person.getAge() < 18 ){
+        youngPeople.add(person);
+        count++;
+        if (count == limit) {
+          break;
+        }
+      }
+      System.out.println(youngPeople);
+    }
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
 
@@ -26,7 +42,9 @@ public class Lecture1 {
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
+    MockData.getPeople().stream()
+        .filter(person -> person.getAge() < 18)
+        .collect(Collectors.toList())
+        .forEach(System.out::println);
   }
 }
